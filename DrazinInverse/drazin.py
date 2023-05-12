d8ad6888f336240424567ae911bbd36749f4871b
Adrian <bloomera@oregonstate.edu>
2023-05-12 23:10:12 +0000

    may 12 lab

--- DrazinInverse/drazin.py
+++ DrazinInverse/drazin.py
@@ -1,8 +1,8 @@
 # drazin.py
 """Volume 1: The Drazin Inverse.
-<Name>
-<Class>
-<Date>
+Adrian Bloomer
+MTH 420
+May 12
 """
 
 import numpy as np
@@ -50,7 +50,15 @@ def is_drazin(A, Ad, k):
     Returns:
         (bool) True of Ad is the Drazin inverse of A, False otherwise.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    return (
+        np.allclose(A @ Ad, Ad @ A) and
+        np.allclose(np.linalg.matrix_power(A, k+1) @ Ad, np.linalg.matrix_power(A, k)) and
+        np.allclose(Ad @ A @ Ad, Ad)
+    )
+
+print(is_drazin(
+    np.array([[1,3,0,0],[0,1,3,0],[0,0,1,3],[0,0,0,0]]),
+    np.array([[1,-3,9,81],[0,1,-3,-18],[0,0,1,3],[0,0,0,0]]), 1))
 
 
 # Problem 2
@@ -63,8 +71,34 @@ def drazin_inverse(A, tol=1e-4):
     Returns:
        ((n,n) ndarray) The Drazin inverse of A.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    n = A.shape[0]
+    f1 = lambda x: abs(x) > tol
+    f2 = lambda x: abs(x) <= tol
+    T1, Q1, k1 = la.schur(A, sort=f1)
+    T2, Q2, k2 = la.schur(A, sort=f2)
+    U = np.hstack((Q1[:,:k1], Q2[:,:n-k1]))
+    Uinv = np.linalg.inv(U)
+    V = Uinv @ A @ U
+    Z = np.zeros_like(A)
+    if k1 != 0:
+        Minv = np.linalg.inv(V[:k1,:k1])
+        Z[:k1,:k1] = Minv
+    return U @ Z @ Uinv
 
+print(drazin_inverse(np.array([[1,3,0,0],[0,1,3,0],[0,0,1,3],[0,0,0,0]])))
+
+def laplacian(A):
+    """Compute the Laplacian matrix of the adjacency matrix A,
+    as well as the second smallest eigenvalue.
+
+    Parameters:
+        A ((n,n) ndarray) adjacency matrix for an undirected weighted graph.
+
+    Returns:
+        L ((n,n) ndarray): the Laplacian matrix of A
+    """
+    D = A.sum(axis=1)    # The degree of each vertex (either axis).
+    return np.diag(D) - A
 
 # Problem 3
 def effective_resistance(A):
@@ -77,7 +111,33 @@ def effective_resistance(A):
         ((n,n) ndarray) The matrix where the ijth entry is the effective
         resistance from node i to node j.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+
+    """
+    n = A.shape[0]
+    L = laplacian(A)
+    I = np.eye(n)
+    R = np.zeros_like(A)
+    for i in range(0,n):
+        for j in range(0,n):
+            if i != j:
+                Lj = L.copy()
+                Lj[j,:] = I[j,:]
+                R[i,j] = drazin_inverse(Lj)[i,i]
+    return R
+    """
+
+    # I couldn't get the algorithm from the book to work so I used an algorihm from Wikipedia instead
+    # https://en.wikipedia.org/wiki/Resistance_distance
+    n = A.shape[0]
+    L = laplacian(A)
+    g = la.pinv(L + np.ones_like(A) / n)
+    d = np.diagonal(g).reshape((n,1))
+    return d @ np.ones((1,n)) + np.ones((n,1)) @ d.T - g - g.T
+
+
+print(effective_resistance(np.array([[0,1,0,0],[1,0,1,0],[0,1,0,1],[0,0,1,0]])))
+print(effective_resistance(np.array([[0,1,1],[1,0,1],[1,1,0]])))
+print(effective_resistance(np.array([[0,3],[3,0],])))
 
 
 # Problems 4 and 5
@@ -110,7 +170,7 @@ class LinkPredictor:
         Raises:
             ValueError: If node is not in the graph.
         """
-        raise NotImplementedError("Problem 5 Incomplete"
+        raise NotImplementedError("Problem 5 Incomplete")
 
 
     def add_link(self, node1, node2):
